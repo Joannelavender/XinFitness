656eda9707d2ed0433f793c2c98fc06be4ae4d33
Joannelavender <kdhgleo@gmail.com>
2022-11-21 03:39:50 +0800

    1121

--- javascript/open.js
+++ javascript/open.js
@@ -1,36 +1,36 @@
-$(function(){
+// $(function(){
 
-    $("#fa-baropen").on("click",function(){
-        $(".top_ul").toggleClass("open_nav");
-    });
-    $("#fa-xmark").on("click",function(){
-        $(".top_ul").toggleClass("open_nav");
-    });
+//     $("#fa-baropen").on("click",function(){
+//         $(".top_ul").toggleClass("open_nav");
+//     });
+//     $("#fa-xmark").on("click",function(){
+//         $(".top_ul").toggleClass("open_nav");
+//     });
 
-    $("#fa-chevron").on("click",function(){
-        $(".top_container").toggleClass("open_topcontainer");
-    });
-    // $(".top_ulitem_close").on("click",function(){
-    //    console.log("ttt");
-    //     // $(".top_container").toggleClass("open_topcontainer");
-    // });
-});
+//     $("#fa-chevron").on("click",function(){
+//         $(".top_container").toggleClass("open_topcontainer");
+//     });
+//     // $(".top_ulitem_close").on("click",function(){
+//     //    console.log("ttt");
+//     //     // $(".top_container").toggleClass("open_topcontainer");
+//     // });
+// });
 
-// $(function () {
-//   $("#fa-baropen").on("click", function () {
-//     $(".top_ul").toggleClass("open_nav");
-//   });
-//   $("#fa-xmark").on("click", function () {
-//     $(".top_ul").toggleClass("open_nav");
-//   });
+$(function () {
+  $("#fa-baropen").on("click", function () {
+    $(".top_ul").toggleClass("open_nav");
+  });
+  $("#fa-xmark").on("click", function () {
+    $(".top_ul").toggleClass("open_nav");
+  });
 
-//   $("#fa-chevron").on("click", function () {
-//     $(".top_container").toggleClass("open_topcontainer");
-//   });
+  $("#fa-chevron").on("click", function () {
+    $(".top_container").toggleClass("open_topcontainer");
+  });
 
   // 看不懂 vu0 先註解
   // $(".top_ulitem_close").on("click",function(){
   //    console.log("ttt");
   //     // $(".top_container").toggleClass("open_topcontainer");
   // });
-// });
+});
