d5b59ac76981da7ac2435692ec94f7c324ed492e
Joannelavender <kdhgleo@gmail.com>
2022-11-21 03:01:55 +0800

    1121

--- javascript/open.js
+++ javascript/open.js
@@ -8,10 +8,29 @@ $(function(){
     });
 
     $("#fa-chevron").on("click",function(){
-        $(".top_container").addClass("open_topcontainer");
+        $(".top_container").toggleClass("open_topcontainer");
     });
-    $(".top_ulitem_close").on("click",function(){
-       console.log("ttt");
-        // $(".top_container").toggleClass("open_topcontainer");
-    });
+    // $(".top_ulitem_close").on("click",function(){
+    //    console.log("ttt");
+    //     // $(".top_container").toggleClass("open_topcontainer");
+    // });
 });
+
+// $(function () {
+//   $("#fa-baropen").on("click", function () {
+//     $(".top_ul").toggleClass("open_nav");
+//   });
+//   $("#fa-xmark").on("click", function () {
+//     $(".top_ul").toggleClass("open_nav");
+//   });
+
+//   $("#fa-chevron").on("click", function () {
+//     $(".top_container").toggleClass("open_topcontainer");
+//   });
+
+  // 看不懂 vu0 先註解
+  // $(".top_ulitem_close").on("click",function(){
+  //    console.log("ttt");
+  //     // $(".top_container").toggleClass("open_topcontainer");
+  // });
+// });
