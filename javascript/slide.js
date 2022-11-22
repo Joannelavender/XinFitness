15f9512fbde0f9085632deaf8ea30ef4cf0effd2
Joannelavender <kdhgleo@gmail.com>
2022-11-22 10:56:28 +0800

    輪播

--- javascript/slide.js
+++ javascript/slide.js
@@ -16,18 +16,18 @@
         });
 
 //第二個輪播
-        // var mySwiper_second = new Swiper(".mySwiper_second", {
-        //     //   cssMode: true, 
-        //     loop:true,
-        //     navigation: {
-        //         nextEl: ".swiper-button-next",
-        //         prevEl: ".swiper-button-prev",
-        //     },
-        //     pagination: {
-        //           clickable: true,
-        //         el: ".swiper-pagination",
-        //       },
-        //       mousewheel: true,
+        var swiper = new Swiper(".mySwiper_second", {
+            //   cssMode: true, 
+            loop:true,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            pagination: {
+                  clickable: true,
+                el: ".swiper-pagination",
+              },
+              mousewheel: true,
           
-        //     });
+            });
    
