ab989d9b8c3e5046e94d857195760122e8ab8638
Joannelavender <kdhgleo@gmail.com>
2022-11-22 16:11:04 +0800

    aos,js

--- javascript/changebg.js
+++ javascript/changebg.js
@@ -2,15 +2,25 @@ const topheader = document.querySelector(".top");
 
 
 
-addEventListener("resize", (e) => {
-  if (window.innerWidth <= 414){
-  }
- else {document.addEventListener("scroll", function () {
-  // console.log(window.scrollY);
-  if (window.scrollY >= 200)
-    topheader.style.backgroundColor = `rgba(250,250,250,1)`;
-  if (window.scrollY < 200) topheader.style.background = `rgba(250,250,250,.5)`;
+// addEventListener("resize", (e) => {
+//   console.log(window.scrollY);
+//   if (window.innerWidth <= 460){
+//   }
+//  else {document.addEventListener("scroll", function () {
+//   // console.log(window.scrollY);
+//   if (window.scrollY >= 200)
+//     topheader.style.backgroundColor = `rgba(250,250,250,1)`;
+//   if (window.scrollY < 200) topheader.style.background = `rgba(250,250,250,.5)`;
+// });}
+//  });
+
+ if (window.innerWidth <= 600){
+}
+else {document.addEventListener("scroll", function () {
+// console.log(window.scrollY);
+if (window.scrollY >= 200)
+  topheader.style.backgroundColor = `rgba(250,250,250,1)`;
+if (window.scrollY < 200) topheader.style.background = `rgba(250,250,250,.5)`;
 });}
- });
 
  
